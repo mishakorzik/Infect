faf6cb5c8fa90421866494dc4733086444c07922
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-19 12:12:18 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+cd
+cd
+apt install python
+apt-get install python
+apt install ruby
+apt-get install ruby
+apt install pip
+apt-get install pip
+pip install requests
+pip2 install requests
+gem install lolcat
+apt install figlet
+apt-get install figlet
+figlet 'Infect installed' | lolcat -p 1.0
