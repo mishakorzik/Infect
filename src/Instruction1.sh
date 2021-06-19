321b2e0d7157717bd21a867ce6d763f2033db9d3
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-06-19 11:42:32 +0300

    Create Instruction1.sh

---
+++ src/Instruction1.sh
@@ -0,0 +1,12 @@
+g="\033[1;32m"
+r="\033[1;31m"
+b="\033[1;34m"
+w="\033[0m"
+o="\033[1;33m"
+
+echo -e $w"["$g"INFO"$w"]"$b"Throw the virus to the victim and tell the victim to download"$w
+sleep 0.5
+echo -e $w"["$g"INFO"$w"]"$b"When downloading, tell the victim to run the virus"$w
+sleep 1
+echo -e $w"["$o"WARN"$w"]"$b"Bye Bye! Have a nice day"$w
+
